05915de7bb8a129e23e24755b5cf50617a2ad8e0
Axel Dolce <axel.dolce@gmail.com>
2020-05-12 00:43:16 -0300

    info

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const mandrill = Mandrill(process.env.API_KEY)
 
 const app = express()
 
+app.get('/message/:id/info', (req, res, next) => {
+    try {
+        const {id} = req.params
+        const data = await mandrill.info(id)
+        res.json(data)
+    } catch (err) {
+        next(err)
+    }
+})
+
 app.get('/message/:id/content', (req, res, next) => {
     try {
         const {id} = req.params
-        const content = await mandrill.content(id)
-        res.send(content.html)
+        const data = await mandrill.content(id)
+        res.send(data.html)
     } catch (err) {
         next(err)
     }
--- mandrill.js
+++ mandrill.js
@@ -4,11 +4,22 @@ async function content(key, id) {
     const ep = 'https://mandrillapp.com/api/1.0/messages/content.json'
     const params = { key, id }
     const { data } = await axios.get(ep, { params })
+    const { html } = data
     return {
-        html: data.html
+        html
+    }
+}
+async function info(key, id) {
+    const ep = 'https://mandrillapp.com/api/1.0/messages/info.json'
+    const params = { key, id }
+    const { data } = await axios.get(ep, { params })
+    const { state } = data
+    return {
+        state
     }
 }
 
 module.exports = key => ({
-    content: id => content(key, id)
+    content: id => content(key, id),
+    info: id => info(key, id),
 })
